073551a481c983318df5e127a482289715360fad
xooseph <josephlsalgado@outlook>
2024-07-12 15:58:23 -0600

    Create Lexer Class 🌨️

--- src/lexer/init.py
+++ src/lexer/init.py
@@ -1,1 +1,12 @@
+from lexer import Lexer
+
 print("Hello, world from Arithmo!)")
+
+# Test the lexer function
+test = "for (var i: int = 0; i < 10; i++) { print(i);})"
+
+lexer = Lexer()
+
+tokens = lexer.tokenize(test)
+
+print(tokens) 
--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,28 +1,27 @@
 from tokens import TOKENS # Import the list of TOKENS
 import re # Import the regular expression module
 
-def lexer(input_string):
-    tokens = []
-    while len(input_string) > 0:
-        match = None # Initialize match to None
-        input_string = input_string.strip(" ") # Remove leading whitespace
-        for token in TOKENS: 
-            name, pattern = token # Unpack the token tuple
-            regex = re.compile(pattern) # Compile the regular expression pattern
-            match = regex.match(input_string) # Match the pattern against the input string
-            if match:
-                value = match.group() # Get the matched value
-                tokens.append((name, value)) # Append the token to the list
-                input_string = input_string[len(value):] # Remove the token from the input string
-                break
-        if not match:
-            raise Exception('Error: unexpected character %s' % input_string[0]) # Raise an exception if an unexpected character is encountered
-    return tokens # Return the list of tokens
+class Lexer:
+    def __init__(self) -> None:
+        pass
 
-# Test the lexer function
-test = "for (var i: int = 0; i < 10; i++) { print(i);})"
-
-print(lexer(test)) 
+    def tokenize(self, input_string):
+        tokens = []
+        while len(input_string) > 0:
+            match = None # Initialize match to None
+            input_string = input_string.strip(" ") # Remove leading whitespace
+            for token in TOKENS: 
+                name, pattern = token # Unpack the token tuple
+                regex = re.compile(pattern) # Compile the regular expression pattern
+                match = regex.match(input_string) # Match the pattern against the input string
+                if match:
+                    value = match.group() # Get the matched value
+                    tokens.append((name, value)) # Append the token to the list
+                    input_string = input_string[len(value):] # Remove the token from the input string
+                    break
+            if not match:
+                raise Exception('Error: unexpected character %s' % input_string[0]) # Raise an exception if an unexpected character is encountered
+        return tokens # Return the list of tokens
 
 """
 Tokenizes the input string by matching it against a list of predefined tokens.
