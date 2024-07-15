280d4758eecbd2f37f258630d8a9fa463a00a7f5
Adayair <adairolegario10agosto@gmail.com>
2024-07-14 22:22:35 -0700

    chore: Update .gitignore to include all __pycache__ directories 🚀

--- src/main.py
+++ src/main.py
@@ -4,22 +4,13 @@ from parser.parser import Parser
 # testcode = "for (var i: int = 0; i < 10; i++) { print(i);})"
 
 testcode = "Hello, world!"
+
 syntax = '''start: WORD","WORD"!"
 
             %import common.WORD   // imports from terminal library
             %ignore " "           // Disregard spaces in text
          '''
 #print(*[token[1] for token in tokens], sep="")
-
-"""
-string = ""
-for token in tokens:
-    if (token[0] == 'IDENTIFIER'):
-        string += " "
-    string += token[1]
-
-#print(string.lstrip())
-"""
 
 lexer = Lexer()
 lexer.add_token(TOKENS)
