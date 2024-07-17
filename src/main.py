05690fe51ef46f49bd586e12f077324a601d063d
Mario <mario03eche@gmail.com>
2024-07-17 17:18:28 -0600

    fix: handle file extension validation correctly

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-from lexer.lexer import Lexer # Import the lexer
-from lexer.tokens import TOKENS # Import the list of TOKENS
-from parser.parser import Parser # Import the parser
-from parser.syntax import SYNTAX # Import the list of SYNTAX
+from lexer.lexer import Lexer   # Import the lexer
+from lexer.tokens import TOKENS   # Import the list of TOKENS
+from parser.parser import Parser   # Import the parser
+from parser.syntax import SYNTAX   # Import the list of SYNTAX
 
-lexer = Lexer() # Create a lexer object
-lexer.add_token(TOKENS) # Add the tokens to the lexer
+lexer = Lexer()  # Create a lexer object
+lexer.add_token(TOKENS)  # Add the tokens to the lexer
 
-route = 'src/input_files/input.ar'
-input_string = lexer.tokenize_file(route)
+directory = 'src/input_files'
+tokens = lexer.tokenize_files_in_directory(directory)
+for token in tokens:
+    print(f"Tipo: {token[0]}, Valor: {token[1]}")
 
+parser = None
 for key in TOKENS.keys():
-    if key == input_string[0][0]:
+    if key == tokens[0][0]:
         parser = Parser(SYNTAX[key])
         break
-    
-print(parser.parsing(input_string)) # Parse the tokens
+print(parser.parsing(tokens))
