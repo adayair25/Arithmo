bee68760aa979b733a2f331776d13fdcfbcce6e3
xooseph <josephlsalgado@outlook.com>
2024-07-17 02:29:16 -0600

    Add syntax 👢

--- src/parser/syntax.py
+++ src/parser/syntax.py
@@ -1,6 +1,7 @@
 from lexer.tokens import TOKENS # Import the list of TOKENS
 
-SYNTAX = f'''
+SYNTAX = {
+    'STATEMENT_WITH_TYPE': f'''
          start: declaration
          declaration: VAR IDENTIFIER COLON TYPES EQUALITY VALUE SEMICOLON
 
@@ -13,5 +14,32 @@ SYNTAX = f'''
          SEMICOLON: /{TOKENS['SEMICOLON']}/
          
          %ignore " "           // Disregard spaces in text
-       '''
+       ''',
+    'STATEMENT_WITHOUT_TYPE': f'''
+         start: declaration
+         declaration: VAR IDENTIFIER EQUALITY VALUE SEMICOLON
 
+         VAR: /{TOKENS['VAR']}/
+         IDENTIFIER: /{TOKENS['IDENTIFIER']}/
+         COLON: /{TOKENS['COLON']}/
+         TYPES: /{TOKENS['TYPES']}/
+         EQUALITY: /{TOKENS['EQUALITY']}/
+         VALUE: /{TOKENS['VALUE']}/
+         SEMICOLON: /{TOKENS['SEMICOLON']}/
+         
+         %ignore " "           // Disregard spaces in text
+       ''',
+    'FUNCTION': f'''
+         start: declaration
+         declaration: FUNCTION IDENTIFIER LPAREN RPAREN LBRACE RBRACE
+
+         FUNCTION: /{TOKENS['FUNCTION']}/
+         IDENTIFIER: /{TOKENS['IDENTIFIER']}/
+         LPAREN: /{TOKENS['LPAREN']}/
+         RPAREN: /{TOKENS['RPAREN']}/
+         LBRACE: /{TOKENS['LBRACE']}/
+         RBRACE: /{TOKENS['RBRACE']}/
+         
+         %ignore " "           // Disregard spaces in text
+       '''
+}
