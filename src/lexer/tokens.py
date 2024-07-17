76d10ca5232db49f60e1d789c427519e5d667ccc
Adayair <adairolegario10agosto@gmail.com>
2024-07-17 00:19:40 -0700

    feat: Update lexer tokens for improved readability and consistency 🚀

--- src/lexer/tokens.py
+++ src/lexer/tokens.py
@@ -1,47 +1,47 @@
 TOKENS = [
-    ('VAR', r'var'),         # variable declaration
-    ('FUNCTION', r'fn'),     # function declaration
-    ('IF', r'if\s'),           # if statement
-    ('ELSE', r'else\s'),       # else statement
-    ('WHILE', r'while\s'),     # while statement
-    ('FOR', r'for\s'),         # for statement
-    ('DO', r'do\s'),           # do statement
-    ('BREAK', r'break\s'),     # break statement
-    ('CASE', r'case\s'),       # case statement
-    ('SWITCH', r'switch\s'),   # switch statement
+    ('VAR', r'var'), # variable declaration
+    ('FUNCTION', r'fn'), # function declaration
+    ('IF', r'if\s'), # if statement
+    ('ELSE', r'else\s'), # else statement
+    ('WHILE', r'while\s'), # while statement
+    ('FOR', r'for\s'), # for statement
+    ('DO', r'do\s'), # do statement
+    ('BREAK', r'break\s'), # break statement
+    ('CASE', r'case\s'), # case statement
+    ('SWITCH', r'switch\s'),# switch statement
     ('CONTINUE', r'continue'), # continue statement
-    ('PRINT', r'print'),       # print statement
-    ('LPAREN', r'\('),         # search for (
-    ('RPAREN', r'\)'),         # search for )
-    ('LBRACE', r'\{\s'),         # search for {
-    ('RBRACE', r'\}'),         # search for }
-    ('LBRACKET', r'\['),       # search for [
-    ('RBRACKET', r'\]'),       # search for ]
-    ('COMMA', r','),           # search for ,
-    ('RETURN', r'return'),     # return statement
-    ('COLON', r':'),           # search for :
-    ('TYPES', r'\b(int|flt|dbl|bool|srg)\b'),   # search for int, float, double, bool
-    ('GREATER', r'>'),         # search for >
-    ('LESS', r'<'),            # search for <
-    ('EQUALITY', r'='),        # search for =
-    ('DOUBLE_EQUAL', r'=='),   # search for ==
-    ('INEQUALITY', r'!='),     # search for !=
-    ('GREATER_EQUAL', r'>='),  # search for >=
-    ('LESS_EQUAL', r'<='),     # search for <=
-    ('PLUS', r'\+'),           # search for +
-    ('MULTIPLY', r'\*'),       # search for *
-    ('DIVIDE', r'/'),          # search for /
-    ('MINUS', r'-'),           # search for -
-    ('AND', r'&&'),            # search for &&
-    ('OR', r'\|\|'),           # search for ||
-    ('NOT', r'!'),             # search for !
-    ('EXPONENT', r'\^'),       # search for ^
-    ('REMAINDER', r'%'),       # search for %
-    ('PLUS_DOUBLE', r'\+\+'),  # search for ++
-    ('VALUE', r'\".*?\"|\d+(\.\d+)?'),  # search for value
-    ('SEMICOLON', r';'),       # search for ;
-    ('IDENTIFIER', r'[a-zA-Z_]\w*'),  # search for variable name
-    ('BOOLEAN', r'\b(true|false)\b')  #search for true, false
+    ('PRINT', r'print'), # print statement
+    ('LPAREN', r'\('), # search for (
+    ('RPAREN', r'\)'), # search for )
+    ('LBRACE', r'\{\s'), # search for {
+    ('RBRACE', r'\}'), # search for }
+    ('LBRACKET', r'\['), # search for [
+    ('RBRACKET', r'\]'), # search for ]
+    ('COMMA', r','), # search for ,
+    ('RETURN', r'return'), # return statement
+    ('COLON', r':'), # search for :
+    ('TYPES', r'(srg|int|flt|dbl|bool)'),   # search for int, float, double, bool
+    ('GREATER', r'>'), # search for >
+    ('LESS', r'<'), # search for <
+    ('EQUALITY', r'='), # search for =
+    ('DOUBLE_EQUAL', r'=='), # search for ==
+    ('INEQUALITY', r'!='), # search for !=
+    ('GREATER_EQUAL', r'>='), # search for >=
+    ('LESS_EQUAL', r'<='), # search for <=
+    ('PLUS', r'\+'), # search for +
+    ('MULTIPLY', r'\*'), # search for *
+    ('DIVIDE', r'/'), # search for /
+    ('MINUS', r'-'), # search for -
+    ('AND', r'&&'), # search for &&
+    ('OR', r'\|\|'), # search for ||
+    ('NOT', r'!'), # search for !
+    ('EXPONENT', r'\^'), # search for ^
+    ('REMAINDER', r'%'), # search for %
+    ('PLUS_DOUBLE', r'\+\+'), # search for ++
+    ('VALUE', r'\".*?\"|\d+(\.\d+)?'), # search for value
+    ('SEMICOLON', r';'), # search for ;
+    ('IDENTIFIER', r'[a-zA-Z_]\w*'), # search for variable name
+    ('BOOLEAN', r'\b(true|false)\b') #search for true, false
     ]
 
 #Transform the tokens into a dictionary
--- src/main.py
+++ src/main.py
@@ -3,37 +3,14 @@ from lexer.tokens import TOKENS # Import the list of TOKENS
 from parser.parser import Parser # Import the parser
 from parser.syntax import SYNTAX # Import the list of SYNTAX
 
-testcode = "var i: int = 000000007654.2345678;" # Test object code
+#testcode = "var i: int = 000000007654.2345678;" # Test object code
 
 lexer = Lexer() # Create a lexer object
 lexer.add_token(TOKENS) # Add the tokens to the lexer
-tokens = lexer.tokenize(testcode) # Tokenize the test code
+#tokens = lexer.tokenize(testcode) # Tokenize the test code
 parser = Parser(SYNTAX) # Create a parser object
-print(parser.parsing(tokens)) # Parse the tokens
-import os
-from lexer.lexer import Lexer
-from lexer.tokens import TOKENS
 
 
-syntax = f'''
-         start: declaration
-         declaration: VAR IDENTIFIER COLON TYPES EQUALITY VALUE SEMICOLON
-
-         VAR: /{TOKENS['VAR']}/
-         IDENTIFIER: /{TOKENS['IDENTIFIER']}/
-         COLON: /{TOKENS['COLON']}/
-         TYPES: /{TOKENS['TYPES']}/
-         EQUALITY: /{TOKENS['EQUALITY']}/
-         VALUE: /{TOKENS['VALUE']}/
-         SEMICOLON: /{TOKENS['SEMICOLON']}/
-
-         %import common.WORD   // importa de la biblioteca terminal
-         %ignore " "           // Ignorar espacios en el texto
-       '''
-
-lexer = Lexer()
-lexer.add_token(TOKENS)
 route = 'input_files/input.ar'
 input_string = lexer.tokenize_file(route)
-
-print(input_string)
+print(parser.parsing(input_string)) # Parse the tokens
