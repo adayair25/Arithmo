104257ced8eed06cc4c67d5ee191207c3abe7d86
otoyusuru <edcentenocontreras@gmail.com>
2024-07-18 22:18:23 -0600

    Update Deriv.py

--- src/Deriv.py
+++ src/Deriv.py
@@ -1,109 +1,109 @@
 """
-Derivada de polinomios
+Polynomial derivative
 """
-class Derivada_poli:
-    def __init__(self,coeficientes,variable):
-        self.coeficientes=coeficientes
+class Poli_Derivative:
+    def __init__(self,coefficients,variable):
+        self.coefficients=coefficients
         self.variable=variable
         termo=''
         term=''
-        for i in range(0,len(coeficientes)):
+        for i in range(0,len(coefficients)):
             if i==0:
-                term=f'{self.coeficientes[i]}+'
-            elif i<len(coeficientes):
-                term+=f'{self.coeficientes[i]}'+f'{self.variable}^{i}+'
+                term=f'{self.coefficients[i]}+'
+            elif i<len(coefficients):
+                term+=f'{self.coefficients[i]}'+f'{self.variable}^{i}+'
             else:
-                term +=f'{self.coeficientes[i]}'+f'{self.variable}^{i}'
+                term +=f'{self.coefficients[i]}'+f'{self.variable}^{i}'
         
-        for j in range(1,len(coeficientes)):
+        for j in range(1,len(coefficients)):
             if j==1:
-                termo=f'{j*self.coeficientes[j]}+'
-            elif j<len(coeficientes)-1:
-                termo+=f'{j*self.coeficientes[j]}'+f'{self.variable}^{j-1}+'
+                termo=f'{j*self.coefficients[j]}+'
+            elif j<len(coefficients)-1:
+                termo+=f'{j*self.coefficients[j]}'+f'{self.variable}^{j-1}+'
             else:
-                termo +=f'{j*self.coeficientes[j]}'+f'{self.variable}^{j-1}'
+                termo +=f'{j*self.coefficients[j]}'+f'{self.variable}^{j-1}'
 
-        return print('la derivada de '+term +'es: '+termo)
+        return print('the derivative of '+term +'is: '+termo)
 """
-Derivada de funciones trigonométricas
-"""
-class Derivada_trig_simple:
-    def __init__(self,funcion,variable,constante=1):
+Derivative of simple trigonometric functions"""
+class Simple_Trig_Derivative:
+    def __init__(self,funcion,variable,constant=1):
         self.funcion=funcion
         self.variable=variable
-        self.constante=constante
-        if constante ==1:
+        self.constant=constant
+        if constant ==1:
             if funcion=='sin':
-                self.equivalente='cos('
-                return  print(self.equivalente+self.variable+')')
+                self.equivalent='cos('
+                return  print(self.equivalent+self.variable+')')
             elif funcion=='cos':
-                self.equivalente='-sen('
-                return  print(self.equivalente+self.variable+')')
+                self.equivalent='-sen('
+                return  print(self.equivalent+self.variable+')')
             elif funcion=='tan':
-                self.equivalente='sec^2('
-                return  print(self.equivalente+self.variable+')')
+                self.equivalent='sec^2('
+                return  print(self.equivalent+self.variable+')')
             elif funcion=='cot':
-                self.equivalente='-csc^2('
-                return  print(self.equivalente+self.variable+')')
+                self.equivalent='-csc^2('
+                return  print(self.equivalent+self.variable+')')
             elif funcion=='sec':
-                self.equivalente='sec('+self.variable+')tan('+self.variable
-                return  print(self.equivalente+')')
+                self.equivalent='sec('+self.variable+')tan('+self.variable
+                return  print(self.equivalent+')')
             elif funcion=='csc':
-                self.equivalente='-scs('+self.variable+')cot('+self.variable
-                return  print(self.equivalente+')')
+                self.equivalent='-scs('+self.variable+')cot('+self.variable
+                return  print(self.equivalent+')')
             else:
-                return print("no es una función trigonométrica")
+                return print("this is not a trigonometric function ")
         else:
             if funcion=='sin':
-                self.equivalente='cos('
-                return  print(f"{self.constante}*"+self.equivalente+self.variable+')')
+                self.equivalent='cos('
+                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
             elif funcion=='cos':
-                self.equivalente='-sen('
-                return  print(f"{self.constante}*"+self.equivalente+self.variable+')')
+                self.equivalent='-sen('
+                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
             elif funcion=='tan':
-                self.equivalente='sec^2('
-                return  print(f"{self.constante}*"+self.equivalente+self.variable+')')
+                self.equivalent='sec^2('
+                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
             elif funcion=='cot':
-                self.equivalente='-csc^2('
-                return  print(f"{self.constante}*"+self.equivalente+self.variable+')')
+                self.equivalent='-csc^2('
+                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
             elif funcion=='sec':
-                self.equivalente='sec('+self.variable+')tan('+self.variable
-                return  print(f"{self.constante}*"+self.equivalente+')')
+                self.equivalent='sec('+self.variable+')tan('+self.variable
+                return  print(f"{self.constant}*"+self.equivalent+')')
             elif funcion=='csc':
-                self.equivalente='-scs('+self.variable+')cot('+self.variable
-                return  print(f"{self.constante}*"+self.equivalente+')')
+                self.equivalent='-scs('+self.variable+')cot('+self.variable
+                return  print(f"{self.constant}*"+self.equivalent+')')
             else:
-                return print("no es una función trigonométrica")
+                return print("this is not a trigonometric function")
 """
-Derivada de funciones exponenciales
+Derivative of exponential functions
 """
-class Derivada_expo:
-    def __init__(self,funcion,variable, constante=1):
+class Derivative_expo:
+    def __init__(self,funcion,variable, constant=1):
         self.funcion=funcion
         self.variable=variable
-        self.constante=constante
-        if constante==1:
+        self.constant=constant
+        if constant==1:
             if funcion=='exp':
-                self.equivalente='exp('+self.variable
-                return  print(self.equivalente+')')
+                self.equivalent='exp('+self.variable
+                return  print(self.equivalent+')')
             elif funcion=='log':
-                self.equivalente='1/'+self.variable
-                return print(self.equivalente)
+                self.equivalent='1/'+self.variable
+                return print(self.equivalent)
         else:
             if funcion=='exp':
-                self.equivalente='exp('+self.variable
-                return  print(f"{self.constante}*"+self.equivalente+')')
+                self.equivalent='exp('+self.variable
+                return  print(f"{self.constant}*"+self.equivalent+')')
             elif funcion=='log':
-                self.equivalente='1/'+self.variable
-                return print(f"{self.constante}*"+self.equivalente)
+                self.equivalent='1/'+self.variable
+                return print(f"{self.constant}*"+self.equivalent)
             else: 
-                return print ("no es una función exponencial/logaritmica")
+                return print ("this is not an exponential, logarithmic function")
             
             
             
         
-poli = Derivada_poli([3,2,1],'x')    
-trigo=Derivada_trig_simple('cos', 'k',1) 
-trigo2=Derivada_trig_simple('son', 'm',1)       
-expo=Derivada_expo('log','x',4)
-expo2=Derivada_expo('logi','s',4)
+poli = Poli_Derivative([3,2,1],'x')    
+trigo=Simple_Trig_Derivative('cos', 'k',4) 
+trigo2=Simple_Trig_Derivative('son', 'm',1)       
+expo=Derivative_expo('log','x',4)
+expo2=Derivative_expo('logi','s',4)
+
