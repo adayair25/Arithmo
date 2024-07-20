eee50ba48ba258b9b2f9d7643d707dc0ffce8f51
otoyusuru <edcentenocontreras@gmail.com>
2024-07-20 14:56:45 -0600

    Update Deriv.py

--- src/Deriv.py
+++ src/Deriv.py
@@ -1,109 +1,113 @@
+# -*- coding: utf-8 -*-
 """
-Polynomial derivative
+Created on Sat Jul 20 13:13:06 2024
+
+@author: COLOR CENTER
 """
-class Poli_Derivative:
-    def __init__(self,coefficients,variable):
-        self.coefficients=coefficients
-        self.variable=variable
-        termo=''
-        term=''
+
+def Poli_Derivative (variable,coefficients=[]):
+
         for i in range(0,len(coefficients)):
             if i==0:
-                term=f'{self.coefficients[i]}+'
+                term=f'{coefficients[i]}+'
             elif i<len(coefficients):
-                term+=f'{self.coefficients[i]}'+f'{self.variable}^{i}+'
+                term+=f'{coefficients[i]}'+f'{variable}^{i}+'
             else:
-                term +=f'{self.coefficients[i]}'+f'{self.variable}^{i}'
+                term +=f'{coefficients[i]}'+f'{variable}^{i}'
         
         for j in range(1,len(coefficients)):
             if j==1:
-                termo=f'{j*self.coefficients[j]}+'
+                Derivative=f'{j*coefficients[j]}+'
             elif j<len(coefficients)-1:
-                termo+=f'{j*self.coefficients[j]}'+f'{self.variable}^{j-1}+'
+                Derivative+=f'{j*coefficients[j]}'+f'{variable}^{j-1}+'
             else:
-                termo +=f'{j*self.coefficients[j]}'+f'{self.variable}^{j-1}'
+                Derivative +=f'{j*coefficients[j]}'+f'{variable}^{j-1}'
+        return Derivative
 
-        return print('the derivative of '+term +'is: '+termo)
+"-------------------------------------------------------------------------------------------------------------------"
 """
 Derivative of simple trigonometric functions"""
-class Simple_Trig_Derivative:
-    def __init__(self,funcion,variable,constant=1):
-        self.funcion=funcion
-        self.variable=variable
-        self.constant=constant
+def  Simple_Trig_Derivative(funcion,variable,constant=1):     
         if constant ==1:
             if funcion=='sin':
-                self.equivalent='cos('
-                return  print(self.equivalent+self.variable+')')
+                equivalent='cos('+variable+')'
+                return equivalent
             elif funcion=='cos':
-                self.equivalent='-sen('
-                return  print(self.equivalent+self.variable+')')
+                equivalent='-sin('+variable+')'
+                return  equivalent
             elif funcion=='tan':
-                self.equivalent='sec^2('
-                return  print(self.equivalent+self.variable+')')
+                equivalent='sec^2('+variable+')'
+                return  equivalent
             elif funcion=='cot':
-                self.equivalent='-csc^2('
-                return  print(self.equivalent+self.variable+')')
+                equivalent='-csc^2('+variable+')'
+                return  equivalent
             elif funcion=='sec':
-                self.equivalent='sec('+self.variable+')tan('+self.variable
-                return  print(self.equivalent+')')
+                equivalent='sec('+variable+')tan('+variable+')'
+                return  equivalent
             elif funcion=='csc':
-                self.equivalent='-scs('+self.variable+')cot('+self.variable
-                return  print(self.equivalent+')')
+                
+                equivalent='-scs('+variable+')cot('+variable+')'
+                return  equivalent
             else:
-                return print("this is not a trigonometric function ")
+                raise ValueError("Is needed a trigonometric function")
         else:
+            '--------------------------------------------------------------------'            
             if funcion=='sin':
-                self.equivalent='cos('
-                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
+                equivalent=f'{constant}'+'*cos('+variable+')'
+                return  equivalent
             elif funcion=='cos':
-                self.equivalent='-sen('
-                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
+                equivalent=f'-{constant}'+'*sin('+variable+')'
+                return  equivalent
             elif funcion=='tan':
-                self.equivalent='sec^2('
-                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
+                equivalent=f'{constant}'+'*sec^2('+variable+')'
+                return  equivalent
             elif funcion=='cot':
-                self.equivalent='-csc^2('
-                return  print(f"{self.constant}*"+self.equivalent+self.variable+')')
+                equivalent=f'-{constant}'+'*csc^2('+variable+')'
+                return  equivalent
             elif funcion=='sec':
-                self.equivalent='sec('+self.variable+')tan('+self.variable
-                return  print(f"{self.constant}*"+self.equivalent+')')
+                equivalent=f'{constant}'+'*sec('+variable+')'+'tan('+variable+')'
+                return  equivalent
             elif funcion=='csc':
-                self.equivalent='-scs('+self.variable+')cot('+self.variable
-                return  print(f"{self.constant}*"+self.equivalent+')')
+                equivalent=f'-{constant}'+'csc('+variable+')cot('+variable+')'
+                return  equivalent
             else:
-                return print("this is not a trigonometric function")
+                raise ValueError("Is needed a trigonometric function")
+
+"-------------------------------------------------------------------------------------------------------------------"
 """
 Derivative of exponential functions
 """
-class Derivative_expo:
-    def __init__(self,funcion,variable, constant=1):
-        self.funcion=funcion
-        self.variable=variable
-        self.constant=constant
+def Derivative_expo(funcion,variable, constant=1):
+
         if constant==1:
             if funcion=='exp':
-                self.equivalent='exp('+self.variable
-                return  print(self.equivalent+')')
+                equivalent='e^'+variable
+                return  equivalent
             elif funcion=='log':
-                self.equivalent='1/'+self.variable
-                return print(self.equivalent)
+                equivalent='1/'+variable
+                return equivalent
         else:
             if funcion=='exp':
-                self.equivalent='exp('+self.variable
-                return  print(f"{self.constant}*"+self.equivalent+')')
+                equivalent=f'{constant}*e^'+variable
+                return  equivalent
             elif funcion=='log':
-                self.equivalent='1/'+self.variable
-                return print(f"{self.constant}*"+self.equivalent)
+                equivalent=f'{constant}*1/'+variable
+                return print(f"{constant}*"+equivalent)
             else: 
-                return print ("this is not an exponential, logarithmic function")
-            
-            
-            
-        
-poli = Poli_Derivative([3,2,1],'x')    
-trigo=Simple_Trig_Derivative('cos', 'k',4) 
-trigo2=Simple_Trig_Derivative('son', 'm',1)       
-expo=Derivative_expo('log','x',4)
-expo2=Derivative_expo('logi','s',4)
+                raise ValueError("Is needed an exponential/logarithmic function")
 
+
+"-------------------------------------------------------------------------------------------------------------------"
+"""
+Derivative of sum, product, Quotient and chain rule
+"""
+def Derivative_gerenal(mode,function1=0,function2=0,function3=0,variable='x'):
+    if mode=='sum':
+        return print('hola')
+    elif mode=='product':
+        return print('hola')
+    elif mode=='quotient':
+        return print('hola')
+    else:
+        raise ValueError("not valid mode")
+   
