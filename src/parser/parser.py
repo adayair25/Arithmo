e1478aba6de07a07b399f8c299d5defce049cb3d
xooseph <josephlsalgado@outlook.com>
2024-07-26 14:37:44 -0600

    feat: Add Interpreter class 😒

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,9 +1,10 @@
 from lark import Lark
 
 class Parser:
-    def __init__(self, syntax, calculate_tree):
-        # self.parser = Lark(syntax, parser='lalr', start="start") # Recibe la grámatica del lenguaje
-          self.parser = Lark(syntax, parser = 'lalr', transformer = calculate_tree)
+    #def __init__(self, syntax, calculate_tree):
+    def __init__(self, syntax):
+         self.parser = Lark(syntax, parser='lalr', start="start") # Recibe la grámatica del lenguaje
+        #  self.parser = Lark(syntax, parser = 'lalr', transformer = calculate_tree)
     # Analiza la cadena o lista de tokens según la sintaxis
     def parsing(self, tokens):
         return self.parser.parse(''.join(token[1] for token in tokens)).pretty() # Devuelve el árbol de análisis
--- src/st/ast.py
+++ src/st/ast.py
@@ -22,3 +22,25 @@ class CalculateTree(Transformer):
             return self.vars[var]
         except KeyError:
             raise Exception("Variable not found: %s" % var)
+        
+
+class Interpreter:
+    def __init__(self) -> None:
+        pass
+
+    def get_result(self, tree):
+        print(tree)
+        method_name = tree.data # Obtiene la data (rule) del objeto Token
+        method = getattr(self, method_name) # Obtiene el método del objeto Interpreter, dependiendo de "rule"
+        return method(tree)
+    
+    def set_var(self, tree): # Busca de entre los nodos, para asignar variables y valores
+        variable_name = None
+        assigned_value = None
+
+        for children in tree.children:
+            if children.type == "IDENTIFIER":
+                variable_name = children.value
+            elif children.type == "VALUE":
+                assigned_value = children.value
+        return variable_name, assigned_value
